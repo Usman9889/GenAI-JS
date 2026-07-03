9f2d5661bd21156d87b95989b50a92488cbd4f64
Usman9889 <usman9889a@gmail.com>
2026-07-03 18:47:36 +0530

    prompting

---
+++ prompting/01_zero.js
@@ -0,0 +1,28 @@
+import OpenAI from "openai";
+import dotenv from "dotenv";
+
+dotenv.config({ path: "../.env" });
+
+const client = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+async function main() {
+  try {
+    const result = await client.chat.completions.create({
+      model: "gpt-4.1",
+      messages: [
+        {
+          role: "user",
+          content: "Tell me a joke.",
+        },
+      ],
+    });
+
+    console.log(result.choices[0].message.content);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+main();
